0335849d1b603d3541217f8a47276a471fe607c6
pdiveesh <diveeshpoli18@gmail.com>
2024-04-27 15:31:57 +0530

    add department model and controller

---
+++ src/main/java/com/example/Login/model/addDepartment.java
@@ -0,0 +1,20 @@
+package com.example.Login.model;
+
+public class addDepartment {
+    private String departmentName,paper;
+
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+    public String getPaper() {
+        return paper;
+    }
+    public void setPaper(String paper) {
+        this.paper = paper;
+    }
+}
